c6010a689942cd39c51cf56f642e1518a96def2e
Rodrigo Zea <zearodrigo37@gmail.com>
2018-05-09 08:55:06 -0600

    Agregando funcionalidades descritas en la hoja

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
